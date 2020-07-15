ee4b2e2a497c545d3f16e33119e72afea08071ad
Jean-Charles Levy <jean-charles.levy@dauphine.eu>
2020-07-15 10:41:01 +0200

    Appel API getAllOffers

--- index.js
+++ index.js
@@ -22,3 +22,35 @@ request.get('https://api.external.thegoodseat.fr/registeruser', {
   }
 
 });
+
+// appel API pour recuperer les prix et attentes
+
+let requete = request.get('https://api.external.thegoodseat.fr/getalloffers', {
+  'auth': {
+    'user': 'username',
+    'pass': 'WkaejKFztN6Tz1cIPi35M7UcvyYwPZcS6ixumJaP'
+  }
+});
+
+
+// Ce cernier commit ne permet pas vraiment de recuperer les prix et attentes,
+// J'avais aussi essaye ce code en-dessous mais j'ai reçu une erreur, 
+// est-ce que tu pourrais me dire ce qu'il ne va pas sur cette partie de code ?
+
+/*
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let requete = new XMLHttpRequest();
+
+const url_login = "https://api.external.thegoodseat.fr/loginuser";
+requete.open("GET", url_login);
+
+requete.send();
+requete.onload = () => {
+	console.log(requete);
+	if(requete.status===200){
+		console.log(JSON.parse(requete.response));
+	}else{
+		console.log('error ${requete.status}')
+	}
+}
+*/
